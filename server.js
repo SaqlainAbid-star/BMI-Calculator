3a4ca99d3a985b6909f2b49e7438ff5c0d6b11bf
SaqlainAbid-star <64516727+SaqlainAbid-star@users.noreply.github.com>
2021-05-17 21:40:27 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const { response } = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+const port = 3000;
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html")
+});
+
+app.post("/",function(req,res){
+    var n1 = Number(req.body.num1)
+    var n2 = Number(req.body.num2)
+    var result = n1/(n2*n2);
+    
+    res.send("The answer is : "+ result)
+});
+
+app.get("/about",function(req,res){
+    res.send("<h1>Saqlain Abid</h1><p>I am a Programmer and developer</pc>")
+});
+
+app.listen(port, function(){
+    console.log(`The server has been started at port ${port}`)
+});
